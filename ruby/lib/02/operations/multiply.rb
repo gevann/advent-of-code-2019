4cf9fdfeb89f34ff75622fe6ba5e38f1e8938c9a
Graeme Nathan <graeme@juul.com>
2019-12-05 12:56:50 -0800

    fixup! Allow alternate opcode language inputs

--- ruby/lib/02/operations/multiply.rb
+++ ruby/lib/02/operations/multiply.rb
@@ -3,7 +3,7 @@ require '02/operations/binary_operation'
 module Operations
   class Multiply < BinaryOperation
     def self.call(tape)
-      BinaryOperation.(tape, lambda { |a, b| a * b })
+      BinaryOperation.(tape, self)
     end
 
     def self.evaluate(a, b)
