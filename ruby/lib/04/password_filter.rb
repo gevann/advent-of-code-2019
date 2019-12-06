53c01fc73e6b59a1dacef6f20a16f79c622b805d
Graeme Nathan <graeme@juul.com>
2019-12-06 12:00:04 -0800

    Create password filter

---
+++ ruby/lib/04/password_filter.rb
@@ -0,0 +1,34 @@
+class PasswordFilter
+  PASSWORD_LENGTH = 6
+
+  def initialize(range)
+    @range = range
+  end
+
+  def call
+    range.select { |number| meets_criteria?(number) }
+  end
+
+  private
+
+  attr_reader :range
+
+  def meets_criteria?(number)
+    digits = number.digits
+
+    Math.log10(number).to_i + 1 == PASSWORD_LENGTH &&
+      monotonic_decrease?(digits) &&
+      repeated_digit?(digits)
+  end
+
+  def monotonic_decrease?(digits)
+    !!digits.inject do |prev, curr|
+      return false unless curr <= prev
+      curr
+    end
+  end
+
+  def repeated_digit?(digits)
+    digits.uniq.length < digits.length
+  end
+end
---
+++ ruby/spec/04/password_filter_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../spec_helper'
+
+require '04/password_filter'
+
+RSpec.describe PasswordFilter do
+  let(:instance) { described_class.new(range) }
+  let(:range) { (138241..674034) }
+
+  describe '.call' do
+    subject { instance.call }
+    it 'returns the filtered results' do
+      expect(subject.length).to eq(1890)
+    end
+  end
+end
