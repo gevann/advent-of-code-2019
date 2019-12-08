1a21eb1d3bd47dae7376c9a533815e21733163ef
Graeme Nathan <graeme@juul.com>
2019-12-08 13:06:44 -0800

    Create output operation

---
+++ ruby/lib/operations/output.rb
@@ -0,0 +1,15 @@
+module Operations
+  N_VALUES = 2
+  class Output
+    def self.call(tape, output_stream: $stdout)
+      _opcode, location = tape.scan(N_VALUES)
+      position = tape.position
+
+      value = tape.move_to(location)
+      output_stream.print(value)
+
+      tape.move_to(position + 1)
+      tape
+    end
+  end
+end
---
+++ ruby/spec/05/operations/output.rb
@@ -0,0 +1,20 @@
+require_relative '../../spec_helper'
+
+require 'operations/output'
+require 'tape'
+
+RSpec.describe Operations::Output do
+  describe '.call' do
+    subject { described_class.call(tape, output_stream: output_stream) }
+    let(:tape) { Tape.new(arr) }
+    let(:arr)  { [1, 3, 3, 'test'] }
+    let(:output_stream) { double('output', print: 'foo') }
+
+    it 'writes to value to the output stream' do
+      expect(output_stream).
+        to receive(:print).with('test')
+
+      subject
+    end
+  end
+end
